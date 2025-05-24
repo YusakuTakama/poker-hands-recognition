824070ce4c6b3250fbb8c794cd60433c17f84a75
YusakuTakama <y.takama.259@nitech.jp>
2025-05-24 20:38:34 +0900

    add classification

--- model/val.py
+++
@@ -1,7 +0,0 @@
-def validate_model(model, args):
-    """Validate the model with the given arguments."""
-    # Run validation
-    results = model.val(data=args.data_path, batch=args.batch_size)
-    # Display results
-    print('Validation results:', results)
-    return results
