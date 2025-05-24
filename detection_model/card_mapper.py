81ab8188deb11e0cf43ae4ec4c31f214f81c64e8
Takumi Fukuzawa <forthune.0517@gmail.com>
2025-05-25 02:23:58 +0900

    change mapper

--- detection_model/card_mapper.py
+++ detection_model/card_mapper.py
@@ -1,7 +1,66 @@
 from app.card import Card, Suit, Rank
 
-def create_card_from_string(card_str: str) -> Card:
+
+id_to_category = {
+    0: "1h",
+    1: "1d",
+    2: "1c",
+    3: "1s",
+    4: "2h",
+    5: "2d",
+    6: "2c",
+    7: "2s",
+    8: "3h",
+    9: "3d",
+    10: "3c",
+    11: "3s",
+    12: "4h",
+    13: "4d",
+    14: "4c",
+    15: "4s",
+    16: "5h",
+    17: "5d",
+    18: "5c",
+    19: "5s",
+    20: "6h",
+    21: "6d",
+    22: "6c",
+    23: "6s",
+    24: "7h",
+    25: "7d",
+    26: "7c",
+    27: "7s",
+    28: "8h",
+    29: "8d",
+    30: "8c",
+    31: "8s",
+    32: "9h",
+    33: "9d",
+    34: "9c",
+    35: "9s",
+    36: "th",
+    37: "td",
+    38: "tc",
+    39: "ts",
+    40: "jh",
+    41: "jd",
+    42: "jc",
+    43: "js",
+    44: "qh",
+    45: "qd",
+    46: "qc",
+    47: "qs",
+    48: "kh",
+    49: "kd",
+    50: "kc",
+    51: "ks"
+}
+
+
+def create_card_from_string(card_id: int) -> Card:
     """Create a Card object from a string representation."""
+    card_str = id_to_category[card_id]
+
     suit_map = {
         "h": Suit.HEARTS,
         "d": Suit.DIAMONDS,
